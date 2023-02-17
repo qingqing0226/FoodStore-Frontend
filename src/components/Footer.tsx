07ee39f06b150a01ca9e78951edc5693c049768c
Qingqing <qingqing.dai94@gmail.com>
2023-02-17 11:18:26 +0100

    add footer with github link and author information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import GitHub from './github.png';
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <footer>
+      <div className='github-box'>
+        <a href="https://github.com/qingqing0226">
+          <img className='github' src={GitHub} alt="github logo" />
+        </a>
+      </div>
+      <div className='author-info'>
+        <div>&#169; Qingqing Dai</div>
+        <div>Developer at Salt</div>
+        <div>Lustgårdsgatan 19, 11251 Stockholm</div>
+      </div>
+    </footer>
   )
 }
 
